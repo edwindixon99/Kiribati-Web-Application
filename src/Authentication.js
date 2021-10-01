2d76f57f81ac7791779c4e990802be9a562c4ac4
Edwin Dixon <edwindixon6@gmail.com>
2021-10-01 20:05:27 +1300

    Log in working redirects new user to /register

--- src/Authentication.js
+++ src/Authentication.js
@@ -1,18 +1,62 @@
 import { useState, useEffect } from 'react'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios'
 import GoogleLogin from 'react-google-login';
+import { useHistory } from "react-router-dom";
+
+
+
+
 
 
 function Authentication() {
-    const [authed, setAuthed] = useState(false)
+  const history = useHistory();
+  const [authed, setAuthed] = useState(false)
 
 
     const responseGoogle = (response) => {
         console.log("Hello")
         console.log(response);
-        console.log(response.dt.Ot);
-        setAuthed(true);
+        console.log(response.tokenId)
+        let idtoken = response.tokenId
+
+
+        async function loginAttempt() {
+          axios({
+          "method": "POST",
+          "url": "http://localhost:4941/api/v1/login",
+          headers: {
+            'Access-Control-Allow-Origin' : '*',
+            'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+            },
+          data: {
+              "idtoken": idtoken
+          }
+          })
+          .then((response) => {
+            setAuthed(true)
+          })
+          .catch((error) => {
+            if (error.response.status == 404) {
+              history.push("/register");
+            }
+            console.log(error)
+          })
+          
+        
+          
+      }
+      loginAttempt()
+      
+
+      // console.log(searchParam)
+      // if (searchParam) {
+      //     fetchTranslations()
+      //     console.log(data)
+      // }
+
+
         
       }
 
