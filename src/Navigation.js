2ffed99fae1369462d3acb21620846bfbb64a68e
edwindixon99 <49737629+edwindixon99@users.noreply.github.com>
2021-10-13 15:19:24 +1300

    added google auth button back

--- src/Navigation.js
+++ src/Navigation.js
@@ -15,7 +15,7 @@ const Navigation = () => (
         <NavLink to='/kiribati' className="nav-link">Kiribati</NavLink>
         <NavLink to='/english' className="nav-link">English</NavLink>
       </Nav>
-    {/* <Authentication /> */}
+    <Authentication />
     </Navbar.Collapse>
 
   </Navbar>
