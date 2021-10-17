bf86d1f539f7b8f92c58fed9ef11dcf9894444f8
edwindixon99 <49737629+edwindixon99@users.noreply.github.com>
2021-10-17 22:28:10 +1300

    post request added to button (still working on it)

--- src/WordPage.js
+++ src/WordPage.js
@@ -18,6 +18,7 @@ function WordPage() {
     const { lang, word } = useParams();
     const [addSelected, setAddSelected] = useState(false);
     const [newWord, setNewWord] = useState("")
+    const [error, setError] = useState(null)
     console.log(lang)
 
     let url = "https://acme.kiribatitranslate.com/api/v1/" + lang;
@@ -26,8 +27,41 @@ function WordPage() {
       e.preventDefault();
       console.log(newWord)
       // setAddSelected(false)
-      
+      async function postTranslation() {
+      axios({
+        "method": "POST",
+        "url": `https://acme.kiribatitranslate.com/api/v1/${lang}/${word}`,
+        headers: {
+          'Access-Control-Allow-Origin' : '*',
+          'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+          'x-authorization':sessionToken
+          },
+        data: {"translation": newWord}
+        })
+        .then((requestResponse) => {
+          setError(null)
+        console.log("finished")
+        })
+        .catch((error) => {
+    
+          if (error.response.status === 400) {
+            setError("translation already exists")
+          }
+          if (error.response.status === 403) {
+            history.push("/");
+            alert("Timed out You need to logout.")
+          }
+        })
+      }
+      if (/^[a-zA-Z0-9]+( [a-zA-Z0-9]+)*$/.test(newWord)){
+        console.log("works")
+        postTranslation()
+      } else {
+        setError("Bad input format")
+      }
+      // postTranslation()    
     }
+  
 
     useEffect(() => {
         async function fetchTranslations() {
@@ -95,13 +129,14 @@ function WordPage() {
                 <h1>{word}</h1>
               </div>
               <div className="col">
-                {addSelected && <div className="row">
+                {addSelected && sessionToken && <div className="row">
                   <form className="form-inline">
-                  <input value={newWord} onChange={e => setNewWord(e.target.value)} className="form-control form-control-lg" type="text" placeholder="Enter Translation" required/>
+                  <input value={newWord} onChange={e => setNewWord(e.target.value)} className="form-control form-control-lg" type="text" placeholder="Enter Translation" pattern="^[a-zA-Z0-9]+( [a-zA-Z0-9]+)*$"/>
                   <button type="submit" className="btn btn-secondary btn-lg" onClick={addTranslation}>Add</button>
                   </form>
+                  {error && <span className="badge badge-danger">Danger</span>};
                   </div>}
-                {!addSelected && <button onClick={() => setAddSelected(true)} className="btn btn-primary btn-lg">Add Translation</button>}
+                {!addSelected && sessionToken && <button onClick={() => setAddSelected(true)} className="btn btn-primary btn-lg">Add Translation</button>}
                 
               </div>
             </div>
